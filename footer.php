e744460abe7041cd6e1ef2e407e749135ed51378
Yannick Makwenge <yannickmakwenge@gmail.com>
2022-04-28 10:47:08 +0100

    update style

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <footer class="footer">
-    <div class="inner-footer">
         <div class="quick-links">
             <ul>
                 <li class="quick-items"><a href="aboutUs.php">Contact Us</a></li>
@@ -8,16 +7,14 @@
             </ul>
         </div>
         <div class="social-media">
-            <ul class="">
+            <ul>
                 <li class="social-icon"><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
                 <li class="social-icon"><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
                 <li class="social-icon"><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
             </ul>
         </div>
-
-    </div>
     <div class="outer-footer">
-        &copy;2021 Copyright Yannick Makwenge. All Rights Reserved.
+        <span>&copy;2021 Copyright Yannick Makwenge. All Rights Reserved.</span>
     </div>
 </footer>
 </body>
